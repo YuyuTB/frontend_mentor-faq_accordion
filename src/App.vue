0af99b7ecf2ec094e5080c131453fa41e1da8c0e
Théo Berry <theo.berry.meleck@gmail.com>
2024-04-11 11:45:12 +0200

    Background added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Faq from "./components/Faq.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="purple-background">
+      <div class="faq-background-image">
+        <Faq />
+      </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .faq-background-image{
+    background-image: url('./assets/images/background-pattern-mobile.svg');
+    width: 100%;
+    height: 230px;
+  }
+  .purple-background{
+    width: 100%;
+    height: 100svh;
+    background-color: #F4E9FA;
+  }
 </style>
